60bb0c717877ae224bdcbfb65481d00915f767a4
Tomaz Bracic <tomaz.bracic@gmail.com>
2016-02-09 22:22:32 +0100

    added some db results on index page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,16 +2,25 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Awesome job guys! This is the index page, of our polls application')
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    output = ", ".join(q.question_text for q in latest_questions)
+
+    return HttpResponse(output)
+
 
 def detail(request, question_id):
     return HttpResponse('This is the detail vew of the question: %s' % question_id)
 
+
+
 def results(request, question_id):
     return HttpResponse('These are the results of the question: %s' % question_id)
+
+
 
 def vote(request, question_id):
     return HttpResponse('Vote on question: %s' % question_id)
