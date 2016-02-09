1b379f81a6ff63d47bd9c64faa671f82916a90a6
Tomaz Bracic <tomaz.bracic@gmail.com>
2016-02-09 22:18:38 +0100

    added views, urls in polls app

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Awesome job guys! This is the index page, of our polls application')
+
+def detail(request, question_id):
+    return HttpResponse('This is the detail vew of the question: %s' % question_id)
+
+def results(request, question_id):
+    return HttpResponse('These are the results of the question: %s' % question_id)
+
+def vote(request, question_id):
+    return HttpResponse('Vote on question: %s' % question_id)
