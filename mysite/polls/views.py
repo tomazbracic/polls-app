c793e7ddc38ca76951479d528619d8d58b2f04af
Tomaz Bracic <tomaz.bracic@gmail.com>
2016-02-09 23:58:57 +0100

    added results page with counting on results

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from .models import Question
 
 # Create your views here.
@@ -19,9 +19,18 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse('These are the results of the question: %s' % question_id)
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse('Vote on question: %s' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except:
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': "Please select a choice"})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
