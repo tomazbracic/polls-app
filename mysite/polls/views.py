8c4b6963c5b74510c20ca8e10393e55314ec443f
Tomaz Bracic <tomaz.bracic@gmail.com>
2016-02-09 22:56:03 +0100

    some refactoring - cleander code

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -10,11 +10,8 @@ from django.template import loader, RequestContext
 
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_questions': latest_questions
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_questions': latest_questions}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
