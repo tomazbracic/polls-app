f87f2e784621ca07eb48853806f4a192fcdcc6db
Tomaz Bracic <tomaz.bracic@gmail.com>
2016-02-09 22:52:14 +0100

    added index.html under polls templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,13 +3,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Question
+
+from django.template import loader, RequestContext
+
 # Create your views here.
 
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
-    output = ", ".join(q.question_text for q in latest_questions)
-
-    return HttpResponse(output)
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_questions': latest_questions
+    })
+    return HttpResponse(template.render(context))
 
 
 def detail(request, question_id):
